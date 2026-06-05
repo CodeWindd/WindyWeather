eb79c14a7a55275d3ad12c2e990fe19143205ac3
CodeWindd <djvorttoriginal@outlook.com>
2026-06-05 07:03:43 -0500

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,3 +1,3 @@
-lconst LocationDefaults = {
-    INIT: { lat: 41.8781, lon: -87.6298, name: "Chicago" }
+const LocationSettings = {
+    INITIAL: { lat: 41.8781, lon: -87.6298, name: "Chicago" }
 };
