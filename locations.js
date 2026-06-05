de7eba6e3e7f5b3c200fa32d53463abf6fdfe39e
CodeWindd <djvorttoriginal@outlook.com>
2026-06-04 21:38:15 -0500

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,4 +1,3 @@
-const LocationSettings = {
-    DEFAULT: { lat: 41.8781, lon: -87.6298, city: "Chicago" },
-    API_GEO: "https://geocoding-api.open-meteo.com/v1/search"
+const LocationStore = {
+    CHICAGO: { lat: 41.8781, lon: -87.6298, city: "Chicago" }
 };
